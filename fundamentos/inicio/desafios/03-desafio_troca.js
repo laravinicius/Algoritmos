e856dc992f7fe71f949c6ebaf6fc63c2a3dd7182
Vinicius Lara <viniciusfelipelara@gmail.com>
2022-07-01 17:25:45 -0300

    Create 03-desafio_troca.js
    desafio troca de valor de variáveis

---
+++ fundamentos/inicio/desafios/03-desafio_troca.js
@@ -0,0 +1,15 @@
+//trocar o valor das variaveis
+let a = 7
+let b = 94
+
+//depois da troca o a = 94 e b = 7
+
+// MINHA TENTATIVA
+let temp
+
+temp = b
+b = a
+a = temp
+
+console.log(a)
+console.log(b)
