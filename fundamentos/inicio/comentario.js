8124bf2bfb36d8c3f882db4f9cea41740b6a3d65
Vinicius Lara <viniciusfelipelara@gmail.com>
2022-07-01 15:40:30 -0300

    Sentenças e blocos de código
    sentenças, blocos, comentátios em js

---
+++ fundamentos/inicio/comentario.js
@@ -0,0 +1,12 @@
+// duas barras server para comentar o código no JS
+
+/* 
+barra e asterísco
+serve para
+comentar em várias linhas
+*/
+
+//console.log é usado para exibir algo na tela do computadort!
+console.log("passo #01")
+console.log("passo #02")
+console.log("passo #03")
