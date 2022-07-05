14acbcb8f29e606be2e06228abc26c7464bb7abe
Vinicius Lara <viniciusfelipelara@gmail.com>
2022-07-05 20:47:33 -0300

    operadores lógicos

--- fundamentos/Operadores/06-logicos_02.js
+++ fundamentos/Operadores/06-logicos_02.js
@@ -1,14 +1,14 @@
-const t1 = false;
-const t2 = false;
+const t1 = true;
+const t2 = true;
 
 let comprarTv50 = t1 && t2;
 console.log('Vamos comprar a Tv de 50"?', comprarTv50); //and
 
 let comprarTv32 = t1 !== t2;
-console.log('Vamos comprar a Tv de 32"?', comprarTv32); //XOR
+console.log('Vamos comprar a Tv de 32"?', comprarTv32); //XOR uma coisa ou outra coisa não os 2
 
 let tomarSorvete = t1 || t2;
-console.log('Vamos comprar sorvete"?', tomarSorvete); //or
+console.log('Vamos comprar sorvete"?', tomarSorvete); //or uma coisa ou outro (podem ser os 2)
 
 let ficarEmCasa = !tomarSorvete;
 console.log('Vamos ficar em casa"?', ficarEmCasa); //not
---
+++ fundamentos/Operadores/desafios/04-Desafio_logico.js
@@ -0,0 +1,14 @@
+// duas expressoes que vao resultar em true (aritméticos, relacionais e lógicos)
+// duas expressoes que vao resultar em false (aritméticos, relacionais e lógicos)
+
+let v1 = 3 + 5 > 2 - 1 && 3 * 2 >= 10 / 2;
+let v2 = 10 % 2 == 50 % 10 || 2 + 3 == 5 - 0;
+
+console.log(v1);
+console.log(v2);
+
+let f1 = 56 - 98 > 100 % 37 || 12 * 9 <= 200 / 2;
+let f2 = 10 * 10 > 5 * 5 !== 5 * 2 > 3 * 2;
+
+console.log(f1);
+console.log(f2);
