a3d9976ab81adf67996bf9c6aae6ef532762fa87
Vinicius Lara <viniciusfelipelara@gmail.com>
2022-07-06 09:20:45 -0300

    if's

---
+++ fundamentos/003-controle/002-if_02.js
@@ -0,0 +1,10 @@
+const nota = 8;
+const comportamento = false;
+
+if (nota >= 7 && comportamento)
+    console.log('Parabéns');
+    
+if (nota <= 7 || !comportamento)
+    console.log('Que pena');
+
+console.log('Fim');
---
+++ fundamentos/003-controle/003-if_03.js
@@ -0,0 +1,8 @@
+const nota = 9.3;
+
+if (nota >= 9) {
+    console.log('Parabéns');
+    console.log('Passou de ano');
+}
+
+console.log('Fim');
