ffbd81172afe01f337d9b9cce4f5eabf5cfda928
Vinicius Lara <viniciusfelipelara@gmail.com>
2022-07-07 21:29:07 -0300

    desafio for

---
+++ fundamentos/003-controle/017-extra_template.js
@@ -0,0 +1,10 @@
+const i = 2;
+const j = 4;
+
+console.log('i=', i,'j=',j);
+console.log('1 + 2 =', 1+2);
+console.log('O valor gerado foi de =', Math.random());
+// podemos concatenar strings com variaveis assim: console.log(`string= ${variavel}`)
+console.log(`i= ${i} j= ${j}`);
+console.log(`1 + 2 = ${1 + 2}`);
+console.log(`O valor gerado foi de = ${Math.random()}`);
---
+++ fundamentos/003-controle/desafios/003-desafio_for.js
@@ -0,0 +1,17 @@
+// recriar essa estrutura sem usar números
+// # 
+// ##
+// ###
+// ####
+// #####
+
+// let linha = '';
+// for (let i = 1; i <=6; i++){
+//     linha += '#';
+//     console.log(linha);
+// }
+
+// exemplo para o exercício, dificilmente será usado
+for (let s = '#'; s != '#######'; s += '#'){
+    console.log(s)
+}
