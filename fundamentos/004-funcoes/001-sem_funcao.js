ddbbfb31c8a39bae06382fe19c377d0e07c94806
Vinicius Lara <viniciusfelipelara@gmail.com>
2022-07-11 20:26:01 -0300

    função

---
+++ fundamentos/004-funcoes/001-sem_funcao.js
@@ -0,0 +1,10 @@
+const d1 = 3;
+const m1 = 1;
+const a1 = 2021;
+
+const d2 = 10;
+const m2 = 5;
+const a2 = 2022;
+
+console.log('Dia:', d1,'Mês:', m1,'Ano:', a1);
+console.log('Dia:', d2,'Mês:', m2,'Ano:', a2);
---
+++ fundamentos/004-funcoes/002-com_funcao_01.js
@@ -0,0 +1,21 @@
+const d1 = 3;
+const m1 = 1;
+const a1 = 2021;
+const d2 = 10;
+const m2 = 5;
+const a2 = 2022;
+
+function exibirData1() {
+    console.log(d1,'/', m1,'/', a1);
+}
+
+function exibirData2() {
+    console.log(d2,'-', m2,'-', a2);
+}
+
+exibirData1();
+exibirData1();
+exibirData1();
+exibirData2();
+exibirData2();
+exibirData2();
