8c7f8989c0cfd1d0235872fe346bce55a5107fa9
Vinicius Lara <viniciusfelipelara@gmail.com>
2022-07-14 10:14:11 -0300

    desafio 01 funções

---
+++ fundamentos/004-funcoes/003-com_funcao_02.js
@@ -0,0 +1,14 @@
+const d1 = 5;
+const m1 = 3;
+const a1 = 2001;
+const d2 = 10;
+const m2 = 5;
+const a2 = 2022;
+
+
+function exibirData(d, m, a) {
+    console.log(d,'/' , m,'/' , a);
+}
+
+exibirData(d1, m1, a1);
+exibirData(d2, m2, a2);
---
+++ fundamentos/004-funcoes/desafios/001-desaifo_funcao.js
@@ -0,0 +1,15 @@
+// 2 parametros -> console.log(+)
+// 2 parametros -> console.log(-)
+
+function soma(n1, n2){
+    console.log(n1 + n2);
+}
+function subtracao(n1, n2){
+    console.log(n1 - n2);
+}
+
+soma(3, 4);
+soma(4, 7);
+subtracao(11, 7);
+subtracao(3, 11);
+
