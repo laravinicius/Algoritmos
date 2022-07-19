c7d7c84bbeac040f8e101532da54883c9f9c845c
Vinicius Lara <viniciusfelipelara@gmail.com>
2022-07-19 14:30:57 -0300

    Create 003-basico_03.js

---
+++ fundamentos/005-arrays/003-basico_03.js
@@ -0,0 +1,15 @@
+console.log(typeof console);
+console.log(typeof console.log);
+
+const numeros = [1, 2, 3];
+
+console.log(typeof numeros);
+
+console.log(numeros);
+
+numeros.push(4); // push serve para adicionar valores ao array
+numeros.push(5);
+numeros.push(10.98);
+
+console.log(numeros);
+console.log(numeros.length); // length serve para ver a quantidade de valores no array
